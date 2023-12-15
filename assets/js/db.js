a16540dabffd85edb4d0247d1135e7c6cc7f4d0a
coatespeter <65430782+coatespeter@users.noreply.github.com>
2023-12-15 07:58:41 +0100

    more questions added to the db.js file

--- assets/js/db.js
+++ assets/js/db.js
@@ -1,161 +1,131 @@
 /*
-  This file contains the data for the quiz.
-  The data is in the form of an array of objects.
-  Each object represents a topic.
-  Each topic has an array of question objects.
-  Each question object has a question, imageUrl, an array of answers and the correct answer.
+This file contains the data for the quiz.
+The data is in the form of an array of objects.
+Each object represents a topic.
+Each topic has an array of question objects.
+Each question object has a question, imageUrl, an array of answers and the correct answer.
 */
 
 export const DATA = [
-  {
-      "topic": "Christmas Trivia",
-      "questions": [
-          {
-              "question": "What is the name of the snowman in Frozen?",
-              "imageUrl": "...",
-              "answers": ["Olaf", "Sven", "Kristoff", "Hans"],
-              "correctAnswer": "Olaf"
-          },
-          {
-              "question": "Which country first started the tradition of putting up a Christmas tree?",
-              "answers": ["Ireland", "Germany", "USA", "Argentina"],
-              "correctAnswer": "Germany"
-          },
-          {
-              "question": "How many Christmas trees are grown in Europe?",
-              "answers": ["100k", "6 million", "60 million", "200 million"],
-              "correctAnswer": "60 million"
-          },
-          {
-              "question": "In which US state would you find the town Santa Claus?",
-              "answers": ["Indiana", "California", "Ohio", "Texas"],
-              "correctAnswer": "Indiana"
-          },
-          {
-              "question": "What is the traditional Christmas meal eaten in Japan?",
-              "answers": ["Ham", "Turkey", "McDonalds", "KFC"],
-              "correctAnswer": "KFC"
-          }
-      ]
-  },
-  {
-      "topic": "Christmas Movies",
-      "questions": [
-          {
-              "question": "How many ghosts show up in A Christmas Carol?",
-              "answers": ["2", "3", "4", "5"],
-              "correctAnswer": "4"
-          },
-          {
-              "question": "What is the name of the Grinch's dog?",
-              "answers": ["Bingo", "Fred", "Spot", "Max"],
-              "correctAnswer": "Max"
-          },
-          {
-              "question": "Who plays the British Prime Minister in Love Actually?",
-              "answers": ["Pierce Brosnan", "Hugh Grant", "Colin Firth", "Gary Oldman"],
-              "correctAnswer": "Hugh Grant"
-          },
-          {
-              "question": "Which real-life department store was Miracle on 34th Street filmed in?",
-              "answers": ["Macy's", "Bloomingdales", "Sears", "Harrods"],
-              "correctAnswer": "Macy's"
-          },
-          {
-              "question": "In Home Alone, where is the family going on holiday?",
-              "answers": ["Bangkok", "Hawaii", "Berlin", "Paris"],
-              "correctAnswer": "Paris"
-          }
-      ]
-  },
-  {
-      "topic": "Christmas Music",
-      "questions": [
-          {
-              "question": "What is the best-selling Christmas single of all time?",
-              "answers": ["White Christmas", "All I Want for Christmas is You", "Last Christmas", "Jingle Bells"],
-              "correctAnswer": "White Christmas"
-          },
-          {
-              "question": "Which Christmas song was originally written for Thanksgiving?",
-              "answers": ["Jingle Bells", "Santa Claus is Coming to Town", "Rudolph the Red-Nosed Reindeer", "Winter Wonderland"],
-              "correctAnswer": "Jingle Bells"
-          },
-          {
-              "question": "Which Christmas song has been covered by the most artists?",
-              "answers": ["Silent Night", "Jingle Bells", "White Christmas", "Santa Claus is Coming to Town"],
-              "correctAnswer": "Silent Night"
-          },
-          {
-              "question": "Which Christmas song was the first to be broadcast from space?",
-              "answers": ["Jingle Bells", "Santa Claus is Coming to Town", "Rudolph the Red-Nosed Reindeer", "Winter Wonderland"],
-              "correctAnswer": "Jingle Bells"
-          },
-          {
-              "question": "Which Christmas song has been covered by the most artists?",
-              "answers": ["Silent Night", "Jingle Bells", "White Christmas", "Santa Claus is Coming to Town"],
-              "correctAnswer": "Silent Night"
-          }
-      ]
-  },
-  {
-      "topic": "Christmas Food",
-      "questions": [
-          {
-              "question": "",
-              "answers": ["", "", "", ""],
-              "correctAnswer": ""
-          },
-          {
-              "question": "",
-              "answers": ["", "", "", ""],
-              "correctAnswer": "KFC"
-          },
-          {
-              "question": "",
-              "answers": ["", "", "", ""],
-              "correctAnswer": ""
-          },
-          {
-              "question": "",
-              "answers": ["", "", "", "a"],
-              "correctAnswer": ""
-          },
-          {
-              "question": "",
-              "answers": ["", "", "", ""],
-              "correctAnswer": ""
-          }
-      ]
-  },
-  {
-      "topic": "Christmas Traditions",
-      "questions": [
-          {
-              "question": "",
-              "answers": ["", "", "", ""],
-              "correctAnswer": ""
-          },
-          {
-              "question": "",
-              "answers": ["", "", "", ""],
-              "correctAnswer": ""
-          },
-          {
-              "question": "",
-              "answers": ["", "", "", ""],
-              "correctAnswer": ""
-          },
-          {
-              "question": "",
-              "answers": ["", "", "", ""],
-              "correctAnswer": ""
-          },
-          {
-              "question": "",
-              "answers": ["", "", "", ""],
-              "correctAnswer": ""
-          }
-      ]
-  }
+{
+    "topic": "Christmas Trivia",
+    "questions": [
+        {
+            "question": "What is the name of the snowman in Frozen?",
+            "imageUrl": "...",
+            "answers": ["Olaf", "Sven", "Kristoff", "Hans"],
+            "correctAnswer": "Olaf"
+        },
+        {
+            "question": "Which country first started the tradition of putting up a Christmas tree?",
+            "answers": ["Ireland", "Germany", "USA", "Argentina"],
+            "correctAnswer": "Germany"
+        },
+        {
+            "question": "How many Christmas trees are grown in Europe?",
+            "answers": ["100k", "6 million", "60 million", "200 million"],
+            "correctAnswer": "60 million"
+        },
+        {
+            "question": "In which US state would you find the town Santa Claus?",
+            "answers": ["Indiana", "California", "Ohio", "Texas"],
+            "correctAnswer": "Indiana"
+        },
+        {
+            "question": "What is the traditional Christmas meal eaten in Japan?",
+            "answers": ["Ham", "Turkey", "McDonalds", "KFC"],
+            "correctAnswer": "KFC"
+        }
+    ]
+},
+{
+    "topic": "Christmas Movies",
+    "questions": [
+        {
+            "question": "How many ghosts show up in A Christmas Carol?",
+            "answers": ["2", "3", "4", "5"],
+            "correctAnswer": "4"
+        },
+        {
+            "question": "What is the name of the Grinch's dog?",
+            "answers": ["Bingo", "Fred", "Spot", "Max"],
+            "correctAnswer": "Max"
+        },
+        {
+            "question": "Who plays the British Prime Minister in Love Actually?",
+            "answers": ["Pierce Brosnan", "Hugh Grant", "Colin Firth", "Gary Oldman"],
+            "correctAnswer": "Hugh Grant"
+        },
+        {
+            "question": "Which real-life department store was Miracle on 34th Street filmed in?",
+            "answers": ["Macy's", "Bloomingdales", "Sears", "Harrods"],
+            "correctAnswer": "Macy's"
+        },
+        {
+            "question": "In Home Alone, where is the family going on holiday?",
+            "answers": ["Bangkok", "Hawaii", "Berlin", "Paris"],
+            "correctAnswer": "Paris"
+        }
+    ]
+},
+{
+    "topic": "Christmas Music",
+    "questions": [
+        {
+            "question": "What is the best-selling Christmas single of all time?",
+            "answers": ["White Christmas", "All I Want for Christmas is You", "Last Christmas", "Jingle Bells"],
+            "correctAnswer": "White Christmas"
+        },
+        {
+            "question": "Which Christmas song was originally written for Thanksgiving?",
+            "answers": ["Jingle Bells", "Santa Claus is Coming to Town", "Rudolph the Red-Nosed Reindeer", "Winter Wonderland"],
+            "correctAnswer": "Jingle Bells"
+        },
+        {
+            "question": "Which Christmas song has been covered by the most artists?",
+            "answers": ["Silent Night", "Jingle Bells", "White Christmas", "Santa Claus is Coming to Town"],
+            "correctAnswer": "Silent Night"
+        },
+        {
+            "question": "Which Christmas song was the first to be broadcast from space?",
+            "answers": ["Jingle Bells", "Santa Claus is Coming to Town", "Rudolph the Red-Nosed Reindeer", "Winter Wonderland"],
+            "correctAnswer": "Jingle Bells"
+        },
+        {
+            "question": "Which Christmas song has been covered by the most artists?",
+            "answers": ["Silent Night", "Jingle Bells", "White Christmas", "Santa Claus is Coming to Town"],
+            "correctAnswer": "Silent Night"
+        }
+    ]
+},
+{
+    "topic": "Christmas Food",
+    "questions": [
+        {
+            "question": "Which traditional Christmas breakfast cocktail is made of two parts champagne to one part orange juice?",
+            "answers": ["Bucks Fizz", "Bellini", "Mimosa", "French 75"],
+            "correctAnswer": "Bucks Fizz"
+        },
+        {
+            "question": "What U.S. President had his own recipe for eggnog?",
+            "answers": ["Abraham Lincoln", "Bill Clinton", "Barack Obama", "George Washington"],
+            "correctAnswer": "George Washington"
+        },
+        {
+            "question": " Which fruit is traditionally placed inside Christmas stockings?",
+            "answers": ["Apple", "Pear", "Orange", "Banana"],
+            "correctAnswer": "Orange"
+        },
+        {
+            "question": "What dessert symbolizes the traditional Yule Log?",
+            "answers": ["Figgy pudding", "Apple tarts", "Buche de Noelle", "Panettone"],
+            "correctAnswer": "Buche de Noelle"
+        },
+        {
+            "question": " Eggnog originated from Anglia, England, but what was it initially?",
+            "answers": ["A breakfast drink", "Baby food", "A Cooking ingredient", "A type of soup"],
+            "correctAnswer": "A breakfast drink"
+        }
+    ]
+},
 ]
