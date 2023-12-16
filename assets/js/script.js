1cc2fe549346df62a0435010dcb57ee21ddbb625
flashdrag <flashdrag@gmail.com>
2023-12-16 11:53:04 +0000

    Add import statement for setDifficulty function and update play button event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,32 @@
 The general script for the all the pages.
 Used init bootstrap tooltips, popovers and modals throughout the site.
 */
+import { setDifficulty } from "./game-storage.js";
 
-const rulesButton = document.getElementById('rulesButton');
-const rulesModal = document.getElementById('rulesModal');
+// Set the difficulty level when the play button is clicked.
+const playButton = document.getElementById("play-btn");
+playButton.addEventListener("click", () => {
+  const difficulty = playButton.dataset.difficulty;
 
-rulesButton.addEventListener('click', openModal);
+  setDifficulty(difficulty);
+  window.location.href = "game.html";
+});
+
+
+const rulesButton = document.getElementById("rulesButton");
+const rulesModal = document.getElementById("rulesModal");
+
+rulesButton.addEventListener("click", openModal);
 
 function openModal() {
-    rulesModal.style.display = 'block';
+  rulesModal.style.display = "block";
 }
 function closeModal() {
-    rulesModal.style.display = 'none';
+  rulesModal.style.display = "none";
 }
 
-window.addEventListener('click', (event) => {
-    if (event.target === rulesModal) {
-        closeModal();
-    }
+window.addEventListener("click", (event) => {
+  if (event.target === rulesModal) {
+    closeModal();
+  }
 });
