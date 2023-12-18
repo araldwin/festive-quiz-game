febe44ab7924a3d14f7c0417d5be9d49a593bbba
flashdrag <flashdrag@gmail.com>
2023-12-18 04:02:45 +0000

    Restyle game page

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,15 +21,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const music = document.getElementById("background-music");
   const soundWrapper = document.getElementById("sound-wrapper");
 
-  soundWrapper.innerHTML = `<i class="fas fa-l fa-volume-mute" style="color: #17bf07;"></i>`;
+  soundWrapper.innerHTML = `<i class="fas fa-l fa-volume-mute"></i>`;
 
   soundWrapper.addEventListener("click", function () {
     // Toggle between play and pause when the sound icon is clicked
     if (music.paused) {
-      soundWrapper.innerHTML = `<i class="fas fa-l fa-volume-up" style="color: #17bf07;"></i>`;
+      soundWrapper.innerHTML = `<i class="fas fa-l fa-volume-up"></i>`;
       music.play();
     } else {
-      soundWrapper.innerHTML = `<i class="fas fa-l fa-volume-mute" style="color: #17bf07;"></i>`;
+      soundWrapper.innerHTML = `<i class="fas fa-l fa-volume-mute"></i>`;
       music.pause();
     }
   });
